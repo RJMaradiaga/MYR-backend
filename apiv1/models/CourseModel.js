499c239467731766bdd0325ed49e3f68d14229a7
Ryan Maradiaga <ryanjosue.rj@gmail.com>
2023-05-29 15:24:06 -0400

    changes to the course model that allows for saving course progress

--- apiv1/models/CourseModel.js
+++ apiv1/models/CourseModel.js
@@ -2,11 +2,18 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 let { Settings } = require('./SceneModel')
 
+// let SaveSchema = new Schema({
+//     'code': String,
+//     'createTime': Date,
+//     'updateTime': Date
+// }, {_id : false});
+
 let LessonSchema = new Schema({
 	'name': { type: String, required: true },
 	'prompt': String,
 	'code': { type: String, required: true },
     'settings': Settings,
+    // 'userCode': [SaveSchema]
 }, {_id : false});
 
 let CourseSchema = new Schema({
